9e502f7c7353ae46d84124333fc8eab096276264
Syed Abdul Gaffar Shakhadri <syed.17.beec@acharya.ac.in>
2021-05-13 14:40:00 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,23 @@
+import torch
+from math import log2
+
+START_TRAIN_AT_IMG_SIZE = 256
+DATASET = 'Dataset'
+CHECKPOINT_GEN = "generator.pth"
+CHECKPOINT_CRITIC = "discriminator.pth"
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+SAVE_MODEL = True
+LOAD_MODEL = False
+LEARNING_RATE = 1e-3
+BATCH_SIZES = [32, 32, 32, 16, 16, 16, 16, 8, 4]
+IMAGE_SIZE = 512
+CHANNELS_IMG = 3
+Z_DIM = 256  # should be 512 in original paper
+IN_CHANNELS = 256  # should be 512 in original paper
+CRITIC_ITERATIONS = 1
+LAMBDA_GP = 10
+NUM_STEPS = int(log2(IMAGE_SIZE / 4)) + 1
+
+PROGRESSIVE_EPOCHS = [50] * len(BATCH_SIZES)
+FIXED_NOISE = torch.randn(8, Z_DIM, 1, 1).to(DEVICE)
+NUM_WORKERS = 4
